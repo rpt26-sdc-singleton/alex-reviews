9fe2b2216e501dfa82b80cb098f233a09bafb284
Grant Taggart <granttaggartsf@gmail.com>
2021-03-04 17:47:43 -0800

    Initial seeding of reviews db complete

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+const db = require('../db/index.js');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+  db.seedReviews(err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Database has been seeded!');
+    }
+  });
   res.send('Hello World!');
 });
 
