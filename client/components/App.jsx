69a22972bfc3b6f4708c9331f6c25e66aa397488
Grant Taggart <granttaggartsf@gmail.com>
2021-03-19 14:05:06 -0700

    Complete reviews rendering

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
+import Reviews from './Reviews.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      courseNumber: 1,
       reviews: []
     };
 
-    this.fetchReviews = this.fetchReviews.bind(this);
-  }
-
-  fetchReviews() {
-    fetch('http://localhost:3007/api/userReviews/1')
-      .then(response => response.json())
-      .then(data => this.setState({ reviews: data }))
-      .catch(error => console.log(error));
   }
 
   componentDidMount() {
-    this.fetchReviews();
+    this._isMounted = true;
+
+    fetch('http://localhost:3007/api/userReviews/1')
+      .then(response => response.json())
+      .then(data => {
+        if (this._isMounted) {
+          this.setState({ reviews: data.reviews });
+        }
+
+      })
+      .catch(error => console.log(error));
+
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
   }
 
   render() {
     return (
-      <div>Reviews go here!</div>
+      <div>
+        <Reviews reviews={this.state.reviews}/>
+      </div>
     );
   }
 }
