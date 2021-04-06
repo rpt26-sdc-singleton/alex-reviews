ca5a4bd6caae3eca348f4bdca3505e2f4efa426c
Grant Taggart <granttaggartsf@gmail.com>
2021-04-05 20:27:40 -0700

    Add more individual review CSS

--- client/components/TotalReviewScore.jsx
+++ client/components/TotalReviewScore.jsx
@@ -8,7 +8,7 @@ const TotalReviewScore = (props) => {
       <div>
         <div className="reviews-overview">
           <div className="total-review-score">{props.totalReviewScore.totalStarScore}</div>
-          <div>
+          <div className="review-box">
             <div className="stars" dangerouslySetInnerHTML={{ __html: props. stars(props.totalReviewScore.totalStarScore) }} />
             <div className="" >{props.totalReviewScore.reviewCount}  reviews</div>
           </div>
