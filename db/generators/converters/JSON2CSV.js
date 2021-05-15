3d85a07441bb15f8c8f3fc8f293704ac8f0c2f38
GalexyN <alexneeeeewin@icloud.com>
2021-05-15 13:06:04 -0700

    completed making json to csv converter for totalreviews

--- db/generators/converters/JSON2CSV.js
+++ db/generators/converters/JSON2CSV.js
@@ -1,17 +1,46 @@
+const { createReadStream, createWriteStream } = require('fs');
 const fs = require('fs').promises;
 
-const convertReviews = filePath => {
+const convertReviews = (filePath) => {};
 
-}
-
-const convertTotalReviews = async filePath => {
+const convertTotalReviews = async (filePath) => {
   let jsonData = await fs.readFile(filePath);
 
   jsonData = JSON.parse(jsonData);
 
   const headers = Object.keys(jsonData[0]);
 
-  console.log(headers)
-}
+  let csvValues = '';
 
-convertTotalReviews('/Users/galexy/Documents/learn-programming/Courses/Hack Reactor/hack-reactor-work/rptImmersive/rpt26-sdc-review-service/db/generators/seededData/totalreviews.json')
+  jsonData.forEach((el) => {
+    let values = Object.values(el);
+    values = values.join(',') + '\n';
+
+    csvValues += values;
+
+    console.log(csvValues);
+  });
+
+  let transformed = headers + '\n' + csvValues;
+  console.log(transformed);
+
+  fs.writeFile(__dirname + '/totalReviewsConverted.csv', transformed, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('done writing csv file!')
+    }
+  })
+};
+
+// try using streams
+
+// const convertTotalReviews = filePath => {
+//   const readStream = fs.createReadStream(filePath);
+//   const writeStream = fs.createWriteStream(__dirname + '/convertedToCSV.csv')
+
+// }
+
+convertTotalReviews(
+  '/Users/galexy/Documents/learn-programming/Courses/Hack Reactor/hack-reactor-work/rptImmersive/rpt26-sdc-review-service/db/generators/seededData/totalreviews.json'
+);
